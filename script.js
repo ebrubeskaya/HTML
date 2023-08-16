11dc502ce28f1f93e00f4aae96068e5eb8f1e4ab
ebrubeskaya <ebrubesk96@outlook.com>
2023-08-16 13:30:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const sonuc = document.getElementById("sonuc");
+const filtre = document.getElementById("filtre");
+const listItems = [];
+getData();
+filtre.addEventListener("input", (e) => filterData(e.target.value));
+async function getData() {
+  const res = await fetch("https://randomuser.me/api/?results=500");
+  const { results } = await res.json();
+  sonuc.innerHTML = "";
+  results.forEach((user) => {
+    const li = document.createElement("li");
+    listItems.push(li);
+    li.innerHTML = `
+<img src="${user.picture.large}" alt="${user.name.first}">
+<div class="user-info">
+<h4>${user.name.first} ${user.name.last}</h4>
+<p>${user.location.city}, ${user.location.country}</p>
+</div>
+`;
+    sonuc.appendChild(li);
+  });
+}
+function filterData(searchTerm) {
+  listItems.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove("hide");
+    } else {
+      item.classList.add("hide");
+    }
+  });
+}
